2d8aeb9b781abaedb6344f50a46c9e00f72b1ab0
Antonioescp <packageinstaller2@gmail.com>
2023-03-05 17:13:36 -0600

    Added a vector concept for vector matrices

--- include/sima/Concepts.hpp
+++ include/sima/Concepts.hpp
@@ -1,6 +1,7 @@
 #ifndef SIMA_CONCEPTS_H
 #define SIMA_CONCEPTS_H
 
+#include <algorithm>
 #include <concepts>
 
 namespace Sima {
@@ -8,6 +9,10 @@ namespace Sima {
     // TODO Document
     template <typename T>
     concept Number = (std::floating_point<T> || std::integral<T>) && !std::same_as<T, char> && !std::same_as<T, bool>;
+
+    // TODO Document
+    template <std::size_t ROWS, std::size_t COLUMNS, std::size_t DIMENSIONS = std::max(ROWS, COLUMNS)> 
+    concept Vector = (ROWS == 1 && COLUMNS == DIMENSIONS) || (ROWS == DIMENSIONS && COLUMNS == 1);
 }
 
 #endif
